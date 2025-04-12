dfac9104d38e9ae05c5fb19e9141f446084999e6
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2025-04-12 23:48:10 +0900

    added block sample code

---
+++ block.rb
@@ -0,0 +1,25 @@
+def call_block
+  puts "Start of method"
+  yield
+  yield
+  puts "End of method"
+end
+
+call_block { puts "In the block" }
+
+# Output:
+# Start of method
+# In the block
+# In the block
+# End of method
+
+def who_says_what
+  yield("Dave", "hello")
+  yield("Andy", "goodbye")
+end
+
+who_says_what{ |person, phrase| puts "#{person} says #{phrase}" }
+
+# Output:
+# Dave says hello
+# Andy says goodbye
