e7c939e20c0763fc9d32b353e1be476f3a48a6c6
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2025-04-13 12:58:34 +0900

    added cgi/render_html_by_cgi.rb

--- cgi/cookie_test.rb
+++ cgi/cookie_test.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/opt/homebrew/bin/ruby
 require "cgi"
 
 COOKIE_NAME = "chocolate_chip"
---
+++ cgi/render_html_by_cgi.rb
@@ -0,0 +1,18 @@
+#!/opt/homebrew/bin/ruby
+require 'cgi'
+
+cgi = CGI.new("html5")
+cgi.out do
+  cgi.html do
+    cgi.head { cgi.title { "This is a test" } } +
+    cgi.body do
+      cgi.form do
+        cgi.hr +
+        cgi.h1 { "A Form: " } +
+        cgi.textarea("get_text") +
+        cgi.br +
+        cgi.submit
+      end
+    end
+  end
+end
