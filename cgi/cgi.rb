4e5081b7acbc9ce8e9e5770768c0c1c3ca117cde
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2025-04-13 12:53:07 +0900

    fix cgi error. delete space from cookie name

---
+++ cgi/cgi.rb
@@ -0,0 +1,4 @@
+require "cgi"
+
+puts CGI.escape("Wao hello/test & yes")
+puts CGI.escapeHTML("a < 100 && b > 200")
---
+++ cgi/cookie_test.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+require "cgi"
+
+COOKIE_NAME = "chocolate_chip"
+
+cgi = CGI.new
+values = cgi.cookies[COOKIE_NAME]
+
+msg = if values.empty?
+  "No cookies found"
+else
+  "Cookies found: #{values[0]}"
+end
+
+cookie = CGI::Cookie.new(COOKIE_NAME, Time.now.to_s)
+cookie.expires = Time.now + 30 * 24 * 3600
+cgi.out("cookie" => cookie) { msg }
