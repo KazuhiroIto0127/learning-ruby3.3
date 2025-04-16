a2d9cd1bb79ebde46332836417d0d190af67891a
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2025-04-16 21:59:37 +0900

    rubocop -a

--- book_in_stock.rb
+++ book_in_stock.rb
@@ -1,13 +1,3 @@
-# frozen_string_literal: true
-
-# Represents a book with an ISBN and a price.
-# This class provides a way to initialize and display book details.
-#
-# @example
-#   book = BookInStock.new("978-3-16-148410-0", 29.99)
-#   puts book.to_s # => "ISBN: 978-3-16-148410-0, Price: 29.99"
-#
-# @see https://ruby-doc.org/core-3.3.0/Class.html Ruby Class Documentation
 class BookInStock
   def initialize(isbn, price)
     @isbn = isbn
